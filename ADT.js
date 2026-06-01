511711b38c1975233a15660d4e25990baa25bf4a
waker_want <he_lost@outlook.com>
2026-06-01 00:00:59 -0400

    Update ADT.js

--- ADT.js
+++ ADT.js
@@ -2,8 +2,7 @@
     you can have properly supported custom js types in Snap!
 */
 if (!window.ADT) {
-    function lisp(o, ...a) {
-        let str = o.map(v => v === "" ? a.shift() : v).join("");
+    function parseLisp(str) {
         let proc = new Process(null, world.childThatIsA(IDE_Morph).stage);
         proc.pushContext();
         return proc.assemble(proc.parseCode(str));
@@ -35,12 +34,12 @@ if (!window.ADT) {
             if (!cls.prototype instanceof this) {
                 throw new Error("expecting a js defined adt type but got " + cls.name);
             }
-            if (cls.proto) return;
+            if (Object.hasOwn(cls,"proto")) return;
             if (cls.prototype.__proto__.constructor !== this)
                 this.init(cls.prototype.__proto__.constructor);
-            let proto = new List(["...", cls.prototype.__proto__.constructor.proto])
+            let proto = new List([new List(["...", cls.prototype.__proto__.constructor.proto])])
             for (let k of Object.getOwnPropertyNames(cls.prototype)) {
-                proto.bind(k, lisp`(call (get __do) (this [object]) ${k} (this [inputs]))`);
+                proto.bind(k, parseLisp(`(call (get __do) (this [object]) ${k} (this [inputs]))`));
             }
             proto.bind("_type", cls.adtType || cls.name);
             cls.proto = proto;
